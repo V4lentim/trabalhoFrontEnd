103d72db0eaf253fc8cbf356c72fe4a2b2a0db40
V4lentim <e.valentimtrescates@gmail.com>
2026-04-06 21:18:23 -0300

    Barra de pesquisa corrigida

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
-
-import logo from "../assets/logo.png"
 import { BarraDePesquisa } from "./BarraDePesquisa"
+import logo from "../assets/logo.png"
 import { BotaoCategoria } from "./BotaoCategoria"
 
 export const Header = (props) => {
 
-     
+
 
     return (
         <header className="fixed top-0 left-0 w-full z-50 transition-all flex items-center justify-between p-2 bg-[#1A1A1A] border-b border-purple-900/50 ">
 
-            <BotaoCategoria setData={props.setData}/>
+            <BotaoCategoria setData={props.setData} />
 
             <div className="flex items-center ">
                 <img src={logo} alt="KeyDash-Logo" className="h-20 w-40" />
             </div>
+
+            <BarraDePesquisa setData={props.setData} />
 
             <div className="flex gap-5   text-x1">
                 <button>❤️</button>
